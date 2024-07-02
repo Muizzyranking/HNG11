6ea498713634b551101d82e20407d626b659ba45
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-07-02 23:00:58 +0100

    removed API key

--- stage-1/app.py
+++ stage-1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -8,13 +9,12 @@ app = Flask(__name__)
 @app.route('/api/hello')
 def hello():
     visitor_name = request.args.get('visitor_name', 'Guest')
-    response = requests.get('https://api64.ipify.org?format=json').json()
-    client_ip = response["ip"]
+    client_ip = request.remote_addr
 
     location_data = requests.get(f'https://ipapi.co/{client_ip}/json/').json()
     city = location_data.get('city')
 
-    api_key = '3b35d42640aec626e8d122a86e2302a0'
+    api_key = os.getenv('OPENWEATHER_API_KEY')
     weather_data = requests.get(
         f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric').json()
     temperature = weather_data['main']['temp']
